2c324539d206d69254f6c71e1e953afe4c91c39e
kscscy <kscscy@gmail.com>
2015-12-30 19:56:10 +0900

    123

---
+++ ytmusic/src/main/java/java76/pms/dao/ReviewDao.java
@@ -0,0 +1,19 @@
+package java76.pms.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import java76.pms.domain.Review;
+
+public interface ReviewDao {
+  List<Review> selectList(Map<String,Object> paramMap);
+  
+  int insert(Review review);
+  
+  int delete(int no);
+  
+  int update(Review review);
+
+  Review selectOne(int no);
+  
+}
