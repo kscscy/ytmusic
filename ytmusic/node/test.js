a3a096058318799bf2d490300734e1717037a1c8
kscscy <kscscy@gmail.com>
2016-01-20 18:50:01 +0900

    node js

--- ytmusic/node/test.js
+++ ytmusic/node/test.js
@@ -1,30 +1,55 @@
-// HTML 출력하기
-// 1) http 라이브러리 객체 임포트
+var express = require('express'),
+	cors = require('cors'),
+	mysql = require('mysql'),
+	http = require('http'),
+  bodyParser = require('body-parser'),
+	app = express();
 
-var http = require('http');
-
-var httpServer = http.createServer(function(request, response) {
-	console.log("요청처리");
-	
-	response.writeHead(200, { // 응답헤더
-		'Content-Type' : 'text/html;charset=UTF-8'
-	});
-	response.write("Hello\n");
-	
-	response.write("<!DOCTYPE html>\n");
-	response.write("<html>\n");
-	response.write("<head>\n");
-	response.write("<meta charset=\"UTF-8\">\n"); // escape문자
-	response.write("<title>테스트</title>\n");
-	response.write("</head>\n");
-	response.write("<body>\n");
-	response.write("<h1>nodejs 테스트</h1>\n");
-	response.write("<p>안녕하세요?!</p>\n");
-	response.write("</body>\n");
-	response.write("</html>\n");
-	
-	response.end();
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'java76',
+  password : '1111',
+  database : 'ytmusic'
 });
 
-httpServer.listen(8989);
-console.log("서버 실행중...");
+connection.connect();
+console.log("연결 되었음.");
+
+app.options('/', cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Content-Type', 'application/json');
+  //res.header(200,{"Content-Type": "application/json"});
+  next();
+});
+
+app.post('/', function (req, res) {
+	connection.query(
+			'SELECT * from member where EMAIL='+mysql.escape(req.body.email), 
+			function(err, rows, fields) { // 서버에서 결과를 받았을 때 호출되는 함수(비동기)
+				if (err) throw err;
+				if (rows.length===0) {
+					console.log("새거");
+					res.json({status:'success'});
+				} else {
+					//console.log("이미존재하는 이메일");
+					res.json({status:'fail'});
+					console.log(rows);
+					//res.send(200,'fail');
+				}
+			});
+  //res.send('Hello World!');
+  
+});
+
+app.get('/', function (req, res) {
+	res.send('get 요청 페이지');
+});
+
+app.listen(8081, function () {
+  console.log('listening on port 8081!');
+});
+
--- ytmusic/src/main/webapp/js/auth/auth.js
+++ ytmusic/src/main/webapp/js/auth/auth.js
@@ -1,12 +1,12 @@
 $(function() {
 	var useremail = $.cookie('useremail');
-  
+
 	if(useremail != undefined) {
-		
+
 		$("#useremail").val(useremail);
 		$("#saveEmail").prop("checked",true);
 	}
-	
+
 	$('#login').click(
 			function(event) {
 				var email = $('#useremail').val();
@@ -14,15 +14,15 @@ $(function() {
 				var checkBox = $('input:checkbox[id="saveEmail"]').is(":checked");
 
 				if($("#saveEmail").prop("checked")) {
-          $.cookie('useremail', email);
-		    //아이디저장 미체크면 쿠키에 정보가 있던간에 삭제
-		    } else {
-		        $.removeCookie("useremail");
-		    }
-				
+					$.cookie('useremail', email);
+					//아이디저장 미체크면 쿠키에 정보가 있던간에 삭제
+				} else {
+					$.removeCookie("useremail");
+				}
+
 				if (email == '' || email == null) {
 					alert('email을 입력하세요');
-					
+
 					$("input[id=useremail]").focus();
 					return false;
 				}
@@ -32,30 +32,30 @@ $(function() {
 					focus.password;
 					return false;
 				}
-				
-				
-				
+
+
+
 				$.post('auth/login.do', {
 					email: email,
 					password: password
-					
+
 				},
-				
+
 				function(resultObj) {
 					var ajaxResult = resultObj.ajaxResult;
 
 					if (ajaxResult.status  == "success") {
-					
+
 						emailSession = ajaxResult.data.email;
 						noSession = ajaxResult.data.mem_no;
-					
+
 						sessionStorage.setItem('emailSession', emailSession);
 						sessionStorage.setItem('noSession', noSession);
 						//$.session.set('emailSession',emailSession);
 						//$.session.set('noSession',noSession);
 						//sessionStorage.setItem("emailsession", emailsession);
 						location.href = "index.html";
-						
+
 					} else {
 						alert("이메일 또는 비밀번호를 확인하세요.");
 					}
@@ -65,47 +65,81 @@ $(function() {
 
 $('#signup').click(
 		function(event) {
-	  	//console.log("세션테스트 : " + sessionStorage.getItem("emailsession"));
-	  	var email = $('#email').val();
-	  	var password = $('#password').val();
-	  	var passwordcheck = $('#passwordcheck').val();
-	  	var regEmail = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-	  	
-	  	if (email == '' || email == null) {
+			//console.log("세션테스트 : " + sessionStorage.getItem("emailsession"));
+			var email = $('#email').val();
+			var password = $('#password').val();
+			var passwordcheck = $('#passwordcheck').val();
+			var regEmail = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+
+			if (email == '' || email == null) {
 				alert('email을 입력하세요');
-      	
+
 				$("input[id=email]").focus();
 				return false;
 			}
-	  	if(!regEmail.test(email)) {
-        alert('유효하지 않은 email입니다');
-        $("input[id=email]").focus();
-        return false;
-  		}
- 			if (password == '' || password == null) {
+			if(!regEmail.test(email)) {
+				alert('유효하지 않은 email입니다');
+				$("input[id=email]").focus();
+				return false;
+			}
+			if (password == '' || password == null) {
 				alert('비밀번호를 입력하세요');
 				$("input[id=password]").focus();
 				focus.password;
 				return false;
 			}
-			
+
 			if (passwordcheck == '' || passwordcheck == null) {
 				alert('비밀번호확인란을 입력하세요');
 				$("input[id=passwordcheck]").focus();
 				return false;
 			}
-			
+
 			/*비밀번호와 비밀번호확인란 같은지 확인*/
 			if (password != passwordcheck){
 				alert("비밀번호와 비밀번호 확인란이 다릅니다.");
 				$("input[id=password]").focus();
 				return false;
 			} 
-	  	
-	  	$.post('member/add.do', {
+
+			$.ajax({
+				url: "http://localhost:8081",
+				type: 'POST',
+				data: {
+					email: email,
+					password: password
+				},
+				dataType:'json',
+				beforeSend: function (xhr) {
+					xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+				}
+			}).done(function(resultObj) {
+				console.log("done??????????");
+				var result = resultObj;
+				if (result.status == "success") {
+					alert("중복검사 통과");
+					
+				} else if (result.status == "fail") {
+					alert("중복된 이메일");
+				}
+				
+				/*
+	      var ajaxResult = resultObj.ajaxResult;
+	      if (ajaxResult.status == "success") {
+	        location.href = "index.html";
+	      } else {
+	        alert("회원가입실패");
+	      }
+	      */
+	    });
+		});
+
+/*
+	  	//$.post('member/add.do', {
+	  	$.post('http://localhost:8081', {
 	  		email: email,
 	  		password: password
-	  		
+
 	  	},
 	     function(resultObj) {
 	      var ajaxResult = resultObj.ajaxResult;
@@ -115,23 +149,27 @@ $('#signup').click(
 	        alert("회원가입실패");
 	      }
 	    },'json');
-	  });
+
+
+ */
+
+
 
 $('#logoutbtn').click(
-		
+
 		function(event) {
 			//var loginSession = $.session.get('loginSession');
-	  	//console.log("로그아웃클릭 : "+loginSession);
-	  	
-	  	 $.getJSON('auth/logout.do',
-	     function(resultObj) {
-	      var ajaxResult = resultObj.ajaxResult;
-	      if (ajaxResult.status == "success") {
-	      	
-	      	$.session.clear();
-	      	$.session.remove('emailSession');
-	      	$.session.remove('noSession');
-	        location.href = "index.html";
-	      }
-	    },'json');
-	  });
+			//console.log("로그아웃클릭 : "+loginSession);
+
+			$.getJSON('auth/logout.do',
+					function(resultObj) {
+				var ajaxResult = resultObj.ajaxResult;
+				if (ajaxResult.status == "success") {
+
+					$.session.clear();
+					$.session.remove('emailSession');
+					$.session.remove('noSession');
+					location.href = "index.html";
+				}
+			},'json');
+		});
